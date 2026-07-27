a93cb28d76cdcbaa85dc0e4ad5b7e6a3cbedc4e6
Kamal <kamalmammadli288@gmail.com>
2026-07-27 14:17:49 +0400

    feat: configure JWT authentication filter chain with stateless sessions

--- src/main/java/org/kamal/library_management/config/SecurityConfig.java
+++ src/main/java/org/kamal/library_management/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package org.kamal.library_management.config;
 
+import lombok.RequiredArgsConstructor;
+import org.kamal.library_management.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,8 +22,9 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().permitAll() // Checkpoint 3-də bu "authenticated()" olacaq
-                );
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
